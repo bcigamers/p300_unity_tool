2b9307ba609e68f98ed52668270ac9296b342198
brianjohannes <33841739+brianjohannes@users.noreply.github.com>
2021-11-10 12:14:08 -0700

    working on running python from unity

--- Assets/RunPython.cs
+++ Assets/RunPython.cs
@@ -10,6 +10,15 @@ public class RunPython : MonoBehaviour
     // Settings for python backend
     public string p300Path = @"C:\Users\brian\Documents\p300\p300_python_fresh\src";
 
+    public bool saveData = true;
+    public string user = "test";
+
+    private string todaysDate;
+    private int dailyFileCount;
+    public string fileName;
+
+    public 
+
     // 
     private float window_start;
     private float window_end;
@@ -40,6 +49,10 @@ public class RunPython : MonoBehaviour
     //2
     public void RunP300Python()
     {
+        // Generate the filename from todays date and the filecount
+
+        // Check to make sure that there is not something saved in the given location
+
         Process cmd = new Process();
         cmd.StartInfo.FileName = "cmd.exe";
         cmd.StartInfo.RedirectStandardInput = true;
@@ -52,7 +65,7 @@ public class RunPython : MonoBehaviour
         cmd.StandardInput.WriteLine("conda activate bci_online");
         cmd.StandardInput.WriteLine("python --version");
         cmd.StandardInput.WriteLine("conda --version");
-        cmd.StandardInput.WriteLine("python erp_online_test.py > test.txt");
+        cmd.StandardInput.WriteLine("python erp_online_test.py"); // this is going to std out in the future with file saving happening within python
 
         cmd.StandardInput.Flush();
         cmd.StandardInput.Close();
